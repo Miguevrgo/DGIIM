0bbeb48be9cd3cdce43dfbebe1001d20bbadc12b
Miguevrgo <93781730+Miguevrgo@users.noreply.github.com>
2022-10-20 14:24:48 +0200

    .
    .

--- Primero/1ºCuatri/FP/II_Descomposicion_Factores_Primos.cpp
+++ Primero/1ºCuatri/FP/II_Descomposicion_Factores_Primos.cpp
@@ -1,6 +1,6 @@
 /****************************************************************************
 	FUNDAMENTOS DE PROGRAMACIÓN
-
+	
 	Miguel Ángel De La Vega Rodríguez    |	Grupo A1
 	
 	
@@ -8,7 +8,7 @@
 	Salidas: 
 	
 	
-
+	
 ****************************************************************************/
 
 #include <iostream> //Inclusión de los recursos de E/S
@@ -25,20 +25,24 @@ int main() //Programa principal
 	do {
         cout << "Introduzca el numero: ";
         cin >> numero;
-    }while(numero<0) //Filtro para que numero>0
+    }while(numero<=0); //Filtro para que numero>0
 	
 	//Cálculos
 
     cout << numero << " Se puede factorizar en numero primos como: ";
-
-    for (int i=2;terminar_bucle != true; i++){
-        while (numero%i == 0){
-            cout << i << " ";
-            numero = numero/i;
-        }
-        if (numero == 1){
-            terminar_bucle = true; //Terminamos el bucle
-        }
+	if (numero==1){
+		cout << 1;
+	}
+	else{
+	    for (int i=2;terminar_bucle != true; i++){
+	        while (numero%i == 0){
+	            cout << i << " ";
+	            numero = numero/i;
+	        }
+	        if (numero == 1){
+	            terminar_bucle = true; //Terminamos el bucle
+	        }
+	    }
     }
 	
 	return 0;
