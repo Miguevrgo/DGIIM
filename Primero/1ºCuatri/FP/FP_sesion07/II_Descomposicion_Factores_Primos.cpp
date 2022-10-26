11a194a6f0fefd8e39c8f33367dac7ca471f4727
Miguevrgo <93781730+Miguevrgo@users.noreply.github.com>
2022-10-26 12:22:55 +0200

    k
    k

--- Primero/1ºCuatri/FP/FP_sesion07/II_Descomposicion_Factores_Primos.cpp
+++ Primero/1ºCuatri/FP/FP_sesion07/II_Descomposicion_Factores_Primos.cpp
@@ -2,10 +2,29 @@
 	FUNDAMENTOS DE PROGRAMACIÓN
 	
 	Miguel Ángel De La Vega Rodríguez    |	Grupo A1
+	Escribir un programa que lea un número entero positivo y muestre su 
+	descomposición en factores primos.
+	Hágalo de manera que se calcule y muestre el resultado de dos maneras:
+	a) La primera (más sencilla) consiste en enumerar todos los primos de la 
+	descomposición (en el caso de que un primo intervenga más de una vez se 
+	mostrará tantas veces como sea preciso). 
+	Por ejemplo:
+	360 = 2 * 2 * 2 * 3 * 3 * 5
+	121 = 11 * 11
+	11 = 11
 	
-	
-	Entradas: 
-	Salidas: 
+	b) La segunda consiste en expresar el resultado como el producto de 
+	potencias (en el caso de que un primo intervenga más de una vez se mostrará
+	una potencia cuya base sea el valor del primo y el exponente será el número 
+	de veces que se repite). Evite potencias triviales (de exponente 1) 
+	escribiendo, en ese caso, únicamente la base (el valor del primo). 
+	Por ejemplo:
+	360 = 2^3 * 3^2 * 5
+	121 = 11^2
+	11 = 11
+
+	Entradas: Valor del numero entero
+	Salidas: Descomposicion en factores primos con dos notaciones
 	
 	
 	
@@ -18,30 +37,79 @@ using namespace std;
 int main() //Programa principal
 {
     int numero;
+    int numerob;
     bool terminar_bucle = false;
-
+    bool terminar_bucle2 = false;
+	int potencia;
 	//Entrada de datos
 	
 	do {
         cout << "Introduzca el numero: ";
         cin >> numero;
     }while(numero<=0); //Filtro para que numero>0
-	
+    
+	numerob = numero;
 	//Cálculos
-
-    cout << numero << " Se puede factorizar en numero primos como: ";
+	
+	/*___________________________Manera a)___________________________________*/
+	
+    cout << numero << " Factorizado manera a) : ";
+    
 	if (numero==1){
 		cout << 1;
 	}
 	else{
 	    for (int i=2;terminar_bucle != true; i++){
 	        while (numero%i == 0){
-	            cout << i << " ";
-	            numero = numero/i;
+	        	numero = numero/i;
+	        	if (numero != 1){
+		            cout << i << " * ";
+	            }
+	            else if (numero ==1){
+	            	cout << i;	
+				}
 	        }
 	        if (numero == 1){
 	            terminar_bucle = true; //Terminamos el bucle
 	        }
+	    }
+    }
+    
+    /*___________________________Manera b)___________________________________*/
+    
+	cout << endl << numerob << " Factorizado manera b) : ";
+	if (numerob==1){
+		cout << 1;
+	}
+	else{
+	    for (int j=2;terminar_bucle2 != true; j++){
+	    	potencia = 0;
+	        while (numerob%j == 0){
+	            potencia++;
+	            numerob = numerob/j;
+	        }
+	        if (numerob !=1){
+		        if (potencia > 1){
+		        	cout << j << "^" << potencia << " * ";	
+				}
+				else if (potencia == 1){
+					cout << j << " * ";
+				}
+		        if (numerob == 1){
+		            terminar_bucle2 = true; //Terminamos el bucle
+	        	}
+	        }
+	        else if (numerob ==1){
+	        	if (potencia > 1){
+		        	cout << j << "^" << potencia;	
+				}
+				else if (potencia == 1){
+					cout << j;
+				}
+		        if (numerob == 1){
+		            terminar_bucle2 = true; //Terminamos el bucle
+	        	}	
+			}
 	    }
     }
 	
--- Primero/1ºCuatri/FP/FP_sesion07/II_Gestion_Menu_Doble.cpp
+++ Primero/1ºCuatri/FP/FP_sesion07/II_Gestion_Menu_Doble.cpp
@@ -1,19 +1,21 @@
 /****************************************************************************
 	FUNDAMENTOS DE PROGRAMACIÓN
-
+	
 	Miguel Ángel De La Vega Rodríguez    |	Grupo A1
-	Escribir un programa en el que se presente un menú principal para que el usuario
-    pueda elegir entre las siguientes opciones:
-        A–>Calcular adición. 
+	Escribir un programa en el que se presente un menú principal para que el
+	usuario pueda elegir entre las siguientes opciones:
+    	A–>Calcular adición. 
         P–>Calcular producto. 
         X–>Salir.
     Si el usuario elige en el menú principal:
     a) Salir, el programa terminará su ejecución.
-    b) Calcular adición se presenta un menú (secundario) para que el usuario pueda elegir entre las siguientes opciones:
+    b) Calcular adición se presenta un menú (secundario) para que el usuario 
+	pueda elegir entre las siguientes opciones:
         S–>Calcular suma. 
         R–>Calcular resta. 
         X–>Salir.
-    c) Calcular producto se presenta un menú (secundario) para que el usuario pueda elegir entre las siguientes opciones:
+    c) Calcular producto se presenta un menú (secundario) para que el usuario 
+	pueda elegir entre las siguientes opciones:
         M–>Calcular multiplicación. 
         D–>Calcular división entera. 
         R–>Calcula resto.
@@ -23,7 +25,6 @@
 	Salidas: Resultado opcion elegida
 	
 	
-
 ****************************************************************************/
 
 #include <iostream> //Inclusión de los recursos de E/S
@@ -32,41 +33,47 @@ using namespace std;
 
 int main() //Programa principal
 {
-    char eleccion;
-    bool salir_bucle = false;
-    bool salir_menu1 = false;
-    bool salir_menu2 = false;
+    char eleccion;  //Opcion a elegir en el menu
+    bool salir_bucle = false; //Condicion salir del programa
+    bool salir_menu1 = false; //Condicion salir menu 1
+    bool salir_menu2 = false; //Condicion salir menu 2
     double numero1;
     double numero2;
     double resultado;
-    bool es_entero;
+    int i;
+	int j;
+	int k;
+	int cociente = 0;
 
 	//Entrada de datos
 
 	// Menu de opciones
     do{
-        cout << "\n\nMenu de Opciones" << endl;        
-        cout << "\t[A] Calcular adicion" << endl; 
-        cout << "\t[P] Calcular producto" << endl;
-        cout << "\t[X] Salir" << endl;
+        cout << "\nMenu de Opciones" << endl;        
+        cout << "[A] Calcular adicion" << endl; 
+        cout << "[P] Calcular producto" << endl;
+        cout << "[X] Salir" << endl;
         cin >> eleccion;
         switch (eleccion){
-            case 'A':
+            case 'A': //Calcular Adicion
                 // Menu de la suma
                 do{
-                    cout << "\n\nMenu de Adicion" << endl;        
-                    cout << "\t[S] Calcular suma" << endl;
-                    cout << "\t[R] Calcular resta" << endl;
-                    cout << "\t[X] Salir" << endl;
+                	salir_menu1 = false; //Reseteo salir menu
+                    cout << "\nMenu de Adicion" << endl;        
+                    cout << "[S] Calcular suma" << endl;
+                    cout << "[R] Calcular resta" << endl;
+                    cout << "[X] Salir" << endl;
+                    cin >> eleccion;
                     switch (eleccion){
-                        case 'S':
+                        case 'S': //Suma
                             cout << "Introduzca el numero 1: ";
                             cin >> numero1;
                             cout << "Introduzca el numero 2: ";
-                            cin >>numero2;
+                            cin >> numero2;
                             resultado = numero1 + numero2;
+                            cout << "Resultado: " << resultado << endl;
                             break;                        
-                        case 'R':
+                        case 'R': //Resta
                             cout << "Introduzca el numero 1: ";
                             cin >> numero1;
                             cout << "Introduzca el numero 2: ";
@@ -74,47 +81,68 @@ int main() //Programa principal
                             resultado = numero1 - numero2;
                             cout << "Resultado: " << resultado << endl;
                             break;
-                        case 'X':
+                        case 'X': //Salir
                             salir_menu1 = true;
                             break;
                         default:
                             cout << "Opcion no valida" << endl;
                             break;
                     }
-                }while(salir_menu1 != true)
+                }while(salir_menu1 != true);
                 break;
-            case 'P':        
+            case 'P': //Calcular Producto       
                 //Menu del producto
                 do{
-                    cout << "\n\nMenu de Producto" << endl;        
-                    cout << "\t[M] Calcular Multiplicacion" << endl;
-                    cout << "\t[D] Calcular Division entera" << endl;
-                    cout << "\t[R] Calcular Resto"
-                    cout << "\t[X] Salir" << endl;
-                    //CREO QUE AQUI HAY QUE PONER UN RESET DEL BOOL
+                	salir_menu2 = false; //Reseteo salir menu
+                    cout << "\nMenu de Producto" << endl;        
+                    cout << "[M] Calcular Multiplicacion" << endl;
+                    cout << "[D] Calcular Division entera" << endl;
+                    cout << "[R] Calcular Resto" << endl;
+                    cout << "[X] Salir" << endl;
+                    cin >> eleccion;
                     switch(eleccion){
-                        case 'M':
-                            cout << "Introduzca el numero 1: ";
-                            cin >> numero1;
-                            cout << "Introduzca el numero 2: ";
-                            cin >> numero2;
-                            resultado = numero1*numero2;
+                        case 'M': //Multiplicacion
+                        	do{
+                        		cout << "Introduzca el numero 1: ";
+                            	cin >> numero1;
+							}while (numero1<0);
+                            do{
+                        		cout << "Introduzca el numero 2: ";
+                            	cin >> numero2;
+							}while (numero2<0);
+                            for (i = 1; i <= (int)numero2; i++){
+                            	resultado+=(int)numero1;
+							}
+                            cout << "Resultado: " << resultado << endl;
+                            resultado = 0; //Reset valor del resultado
+                            break;
+                        case 'D': //Division entera
+                            do{
+                        		cout << "Introduzca el numero 1: ";
+                            	cin >> numero1;
+							}while (numero1<0);
+                            do{
+                        		cout << "Introduzca el numero 2: ";
+                            	cin >> numero2;
+							}while (numero2<0);
+                            for (k=(int)numero1;k>=0;k = k-(int)numero2){
+                            	cociente++;
+							}
+							resultado = cociente-1;
                             cout << "Resultado: " << resultado << endl;
                             break;
-                        case 'D':
-                            cout << "Introduzca el numero 1: ";
-                            cin >> numero1;
-                            cout << "Introduzca el numero 2: ";
-                            cin >> numero2;
-                            resultado = (int)numero1/(int)numero2;
-                            cout << "Resultado: " << resultado << endl;
-                            break;
-                        case 'R':
-                            cout << "Introduzca el numero 1: ";
-                            cin >> numero1;
-                            cout << "Introduzca el numero 2: ";
-                            cin >> numero2;
-                            resultado = (int)numero1%(int)numero2;
+                        case 'R': //Resto
+                            do{
+                        		cout << "Introduzca el numero 1: ";
+                            	cin >> numero1;
+							}while (numero1<0);
+                            do{
+                        		cout << "Introduzca el numero 2: ";
+                            	cin >> numero2;
+							}while (numero2<0);
+                            for (j=(int)numero1; j>=0 ;j = j-(int)numero2){
+                            	resultado = j;
+							}
                             cout << "Resultado: " << resultado << endl;
                             break;
                         case 'X':
@@ -124,7 +152,7 @@ int main() //Programa principal
                             cout << "Opcion no valida";
                             break;
                     }
-                }while(salir_menu2!=true)
+                }while(salir_menu2!=true);
                 break;
             case 'X':
                 salir_bucle=true;
@@ -133,15 +161,9 @@ int main() //Programa principal
                 cout << "Opcion no valida" << endl;
                 break;
         }
-    while(salir_bucle!=true)
-	//Cálculos
+    }while(salir_bucle!=true);
 
-
-
-	//Salida
+	cout << "Has salido del menu";
 	
-
-
 	return 0;
 }
-
