aa0f2c584573c6086d7d05e3e5ecdfba01b3999e
Miguevrgo <93781730+Miguevrgo@users.noreply.github.com>
2022-10-29 19:50:59 +0200

    Add files via upload

---
+++ Primero/1ºCuatri/FP/FP_sesion08/III_FormateaString_Funcs.cpp
@@ -0,0 +1,58 @@
+/****************************************************************************
+	FUNDAMENTOS DE PROGRAMACIÓN
+
+	Miguel Ángel De La Vega Rodríguez    |	Grupo A1
+	Escriba una función para dar formato a un string, obteniendo como resultado 
+	un nuevo string de acuerdo al formato indicado en su llamada. La función 
+	tendrá el prototipo:
+
+	string FormateaString (string cad, int num_casillas,
+	TipoAjuste ajuste, char relleno=’ ’);
+	
+	Transforma el contenido del dato cad a un nuevo string de acuerdo al
+	formato indicado. La función devuelve un nuevo string.
+	El nuevo string tendrá un mínimo de num_casillas casillas. Si el número
+	de casillas solicitado (num_casillas) es mayor que el tamaño de cad se
+	completan las casillas necesarias con el carácter relleno. Si la longitud 
+	de cad fuera mayor simplemente se devuelve una copia de cad.
+	Respecto al ajuste se usa un valor del tipo TipoAjuste para indicar la
+	alineación de la cadena resultado. Use la siguiente definición de tipo 
+	enumerado TipoAjuste:
+	enum class TipoAjuste {izquierda, centro, derecha};
+
+	Entradas: 
+	Salidas: 
+	
+	
+
+****************************************************************************/
+
+#include <iostream> //Inclusión de los recursos de E/S
+
+using namespace std;
+
+string FormateaString (string cad, int num_casillas,
+	TipoAjuste ajuste, char relleno=''){
+		
+	}
+	
+int main() //Programa principal
+{
+
+
+	//Entrada de datos
+	
+	cout << ;
+    cin >> ;
+	
+	//Cálculos
+
+
+
+	//Salida
+	
+
+
+	return 0;
+}
+
---
+++ Primero/1ºCuatri/FP/FP_sesion08/III_LecturaEnteros_Funcs.cpp
@@ -0,0 +1,65 @@
+/****************************************************************************
+	FUNDAMENTOS DE PROGRAMACIÓN
+
+	Miguel Ángel De La Vega Rodríguez    |	Grupo A1
+	Escribir las siguientes funciones para leer números enteros:
+	
+	int LeeEntero (string titulo)
+	Lee y devuelve un dato int. Si el valor leido no fuera un int (p.e. 94.5,
+	dos, 77 88, 0xA2, 55.0) la función volvería a pedirlo hasta que se 
+	proporcione un valor correcto. La lectura se etiqueta con titulo.
+	Por ejemplo, si x es int, la ejecución de la instrucción
+	x = LeeEntero("Introduzca la coordenada x: "); muestra Introduzca la 
+	coordenada x: _ y espera a la introducción de un valor int. Si el valor 
+	introducido no fuera int vuelve a pedirlo.
+	
+	int LeeEnteroEnRango (string titulo, int menor, int mayor)
+	Lee y devuelve un dato int en un rango dado. Si el valor leido no fuera int
+	ó éste fuera menor que menor o mayor que mayor la función volvería a
+	pedirlo hasta que se proporcionara un valor correcto. La lectura se etiqueta
+	con titulo.
+	
+	int LeeEnteroMayorOIgual (string titulo, int referencia)
+	Lee y devuelve un dato int que debe ser mayor o igual que referencia.
+	Si el valor leido no fuera int ó éste fuera menor que referencia la función
+	volvería a pedirlo hasta que se proporcionara un valor correcto. La lectura se
+	etiqueta con titulo.
+	
+	Entradas: 
+	Salidas: 
+	
+	
+
+****************************************************************************/
+
+#include <iostream> //Inclusión de los recursos de E/S
+
+using namespace std;
+
+int LeeEntero (string titulo){
+}
+int LeeEnteroEnRango (string titulo, int menor, int mayor){
+}
+int LeeEnteroMayorOIgual (string titulo, int referencia){
+}
+
+int main() //Programa principal
+{
+	string titulo;
+
+	//Entrada de datos
+	
+	cout << ;
+    cin >> ;
+	
+	//Cálculos
+
+
+
+	//Salida
+	
+
+
+	return 0;
+}
+
